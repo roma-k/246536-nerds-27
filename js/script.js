14c37a633dc0ad3a2f67e25ad5a76c72279822c9
Roman <intere07@ya.ru>
2020-04-22 23:12:22 +0700

    цикл
    поробовал сделать цикл

--- js/script.js
+++ js/script.js
@@ -11,20 +11,30 @@ close.onclick = function() {
   postWindow.classList.add('visually-hidden');
 };
 
-storeItem[0].onmouseover = function() {hiddenProdInf[0].classList.remove('visually-hidden');};
-storeItem[0].onmouseout = function () {hiddenProdInf[0].classList.add('visually-hidden');};
+//  storeItem[0].onmouseover = function() {hiddenProdInf[0].classList.remove('visually-hidden');};
+//  storeItem[0].onmouseout = function () {hiddenProdInf[0].classList.add('visually-hidden');};
+//
+// storeItem[0].onmouseover = function() {hiddenProdInf[0].classList.remove('visually-hidden');};
+// storeItem[0].onmouseout = function () {hiddenProdInf[0].classList.add('visually-hidden');};
+//
+// storeItem[0].onmouseover = function() {hiddenProdInf[0].classList.remove('visually-hidden');};
+// storeItem[0].onmouseout = function () {hiddenProdInf[0].classList.add('visually-hidden');};
+//
+// storeItem[0].onmouseover = function() {hiddenProdInf[0].classList.remove('visually-hidden');};
+// storeItem[0].onmouseout = function () {hiddenProdInf[0].classList.add('visually-hidden');};
+//
+// storeItem[0].onmouseover = function() {hiddenProdInf[0].classList.remove('visually-hidden');};
+// storeItem[0].onmouseout = function () {hiddenProdInf[0].classList.add('visually-hidden');};
+//
+// storeItem[0].onmouseover = function() {hiddenProdInf[0].classList.remove('visually-hidden');};
+// storeItem[0].onmouseout = function () {hiddenProdInf[0].classList.add('visually-hidden');};
+storeItem.onmouseout = function () {
+  for (i = 0, i <= storeItem.length) {
+    hiddenProdInf[0].classList.add('visually-hidden');
+    i++;
+  }
+}
 
-storeItem[1].onmouseover = function() {hiddenProdInf[1].classList.remove('visually-hidden');};
-storeItem[1].onmouseout = function () {hiddenProdInf[1].classList.add('visually-hidden');};
 
-storeItem[2].onmouseover = function() {hiddenProdInf[2].classList.remove('visually-hidden');};
-storeItem[2].onmouseout = function () {hiddenProdInf[2].classList.add('visually-hidden');};
 
-storeItem[3].onmouseover = function() {hiddenProdInf[3].classList.remove('visually-hidden');};
-storeItem[3].onmouseout = function () {hiddenProdInf[3].classList.add('visually-hidden');};
 
-storeItem[4].onmouseover = function() {hiddenProdInf[4].classList.remove('visually-hidden');};
-storeItem[4].onmouseout = function () {hiddenProdInf[4].classList.add('visually-hidden');};
-
-storeItem[5].onmouseover = function() {hiddenProdInf[5].classList.remove('visually-hidden');};
-storeItem[5].onmouseout = function () {hiddenProdInf[5].classList.add('visually-hidden');};
